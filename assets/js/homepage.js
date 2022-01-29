58b0cef4b86c26857fa0e8c79731b04b8caf945b
SSto <95592688+sstover25@users.noreply.github.com>
2022-01-28 20:50:34 -0600

    updated request logic with dynamic variables

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,13 @@
-var getUserRepo = function() {
-    fetch("https://api.github.com/users/octocat/repos");
+var getUserRepo = function(user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
 };
 
-getUserRepo();
+getUserRepo("sstover25");
